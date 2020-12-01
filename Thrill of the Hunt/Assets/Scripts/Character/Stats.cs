91feeac98697fadfa62bf13a73d8e134755d438a
Andy Lam <andy.onlam@gmail.com>
2020-12-01 06:57:41 -0500

    Turn Manager

---
+++ Thrill of the Hunt/Assets/Scripts/Character/Stats.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class Stats : MonoBehaviour
+{
+    [SerializeField] string characterName;
+    [Header("Basic")]
+
+    [SerializeField] int damage;
+    [SerializeField] int moveSpeed;
+
+    [Header("Attributes")]
+    [SerializeField] int defense;
+    [SerializeField] int nimbleness;
+    [SerializeField] int brawn;
+    [SerializeField] int brain;
+    [SerializeField] int vigor;
+    UnityEvent onHurt, onDie;
+
+    [Header("UI")]
+    [SerializeField] Sprite Portrait;
+
+    public int getDefense() { return defense; }
+    public int getNimBleness() { return nimbleness; }
+    public int getBrawn() { return brawn; }
+    public int getBrain() { return brain; }
+    public int getVigor() { return vigor; }
+
+    private void Awake()
+    {
+        //Debug
+        nimbleness = Random.Range(1, 20);
+        GameObject.Find("TurnOrder").GetComponent<TurnOrder>().testList.Add(this.gameObject);
+    }
+
+    public Sprite GetSprite()
+    {
+        return Portrait;
+    }
+}
