bcb5a12018963c1d58a571f54b91826c3104826e
DarkScooter <scottoweinsmith@gmail.com>
2020-12-01 08:34:31 -0500

    Inventory and blockout
    test textures as well

---
+++ Thrill of the Hunt/Assets/Scripts/ScriptableObjects/Scripts/EqupmentObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Equipment Object", menuName = "Inventory System/Items/Equipment")]
+public class EqupmentObject : ItemObject
+{
+    public float AtkBonus;
+    public float DefBonus;
+    public void Awake()
+    {
+        itemtype = Itemtype.Equipment;
+    }
+}
---
+++ Thrill of the Hunt/Assets/Scripts/ScriptableObjects/Scripts/HealingObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Healing Object", menuName = "Inventory System/Items/Health")]
+public class HealingObject : ItemObject
+{
+    public int restoreHealthValue;
+    public void Awake()
+    {
+        itemtype = Itemtype.Consumables;      
+    }
+    
+}
