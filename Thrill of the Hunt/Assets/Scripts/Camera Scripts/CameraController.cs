ef2dfbf0b673a1690a359369377dc900dc3f2a23
Chris Kennedy <ChrisKennedy8354@gmail.com>
2020-12-06 14:27:53 -0500

    Camera now has height bounds

--- Thrill of the Hunt/Assets/Scripts/Camera Scripts/CameraController.cs
+++ Thrill of the Hunt/Assets/Scripts/Camera Scripts/CameraController.cs
@@ -9,82 +9,90 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // For Camera moving Forward and backwards
-        if(Input.GetButton("Forward") && PauseMenuManager.gamePause == false)
+        if (PauseMenuManager.gamePause == false)
         {
-            int dir = ((Input.GetAxisRaw("Forward") > 0) ? 1 : -1);
+            // For Camera moving Forward and backwards
+            if (Input.GetButton("Forward"))
+            {
+                int dir = ((Input.GetAxisRaw("Forward") > 0) ? 1 : -1);
 
-            Vector3 direction = transform.forward;
-            direction.y = 0;
-            direction.Normalize();
+                Vector3 direction = transform.forward;
+                direction.y = 0;
+                direction.Normalize();
 
-            transform.Translate(direction * Time.deltaTime * panSpeed * dir, Space.World);
-        }
-        else if(Input.mousePosition.y >= Screen.height * 0.95 && PauseMenuManager.gamePause == false)
-        {
-            int dir = 1;
+                transform.Translate(direction * Time.deltaTime * panSpeed * dir, Space.World);
+            }
+            else if (Input.mousePosition.y >= Screen.height * 0.95)
+            {
+                int dir = 1;
 
-            Vector3 direction = transform.forward;
-            direction.y = 0;
-            direction.Normalize();
+                Vector3 direction = transform.forward;
+                direction.y = 0;
+                direction.Normalize();
 
-            transform.Translate(direction * Time.deltaTime * panSpeed * dir, Space.World);
-        }
-        else if(Input.mousePosition.y <= Screen.height * 0.05 && PauseMenuManager.gamePause == false)
-        {
-            int dir = -1;
+                transform.Translate(direction * Time.deltaTime * panSpeed * dir, Space.World);
+            }
+            else if (Input.mousePosition.y <= Screen.height * 0.05)
+            {
+                int dir = -1;
 
-            Vector3 direction = transform.forward;
-            direction.y = 0;
-            direction.Normalize();
+                Vector3 direction = transform.forward;
+                direction.y = 0;
+                direction.Normalize();
 
-            transform.Translate(direction * Time.deltaTime * panSpeed * dir, Space.World);
-        }
+                transform.Translate(direction * Time.deltaTime * panSpeed * dir, Space.World);
+            }
 
-        // For Camera Moving left and right
-        if (Input.GetButton("Horizontal") && PauseMenuManager.gamePause == false)
-        {
-            int dir = ((Input.GetAxisRaw("Horizontal") > 0) ? 1 : -1);
+            // For Camera Moving left and right
+            if (Input.GetButton("Horizontal"))
+            {
+                int dir = ((Input.GetAxisRaw("Horizontal") > 0) ? 1 : -1);
 
-            Vector3 direction = transform.right;
-            direction.y = 0;
-            direction.Normalize();
+                Vector3 direction = transform.right;
+                direction.y = 0;
+                direction.Normalize();
 
-            transform.Translate(direction * Time.deltaTime * panSpeed * dir, Space.World);
-        }
-        else if (Input.mousePosition.x >= Screen.width * 0.95 && PauseMenuManager.gamePause == false)
-        {
-            int dir = 1;
+                transform.Translate(direction * Time.deltaTime * panSpeed * dir, Space.World);
+            }
+            else if (Input.mousePosition.x >= Screen.width * 0.95)
+            {
+                int dir = 1;
 
-            Vector3 direction = transform.right;
-            direction.y = 0;
-            direction.Normalize();
+                Vector3 direction = transform.right;
+                direction.y = 0;
+                direction.Normalize();
 
-            transform.Translate(direction * Time.deltaTime * panSpeed * dir, Space.World);
-        }
-        else if (Input.mousePosition.x <= Screen.width * 0.05 && PauseMenuManager.gamePause == false)
-        {
-            int dir = -1;
+                transform.Translate(direction * Time.deltaTime * panSpeed * dir, Space.World);
+            }
+            else if (Input.mousePosition.x <= Screen.width * 0.05)
+            {
+                int dir = -1;
 
-            Vector3 direction = transform.right;
-            direction.y = 0;
-            direction.Normalize();
+                Vector3 direction = transform.right;
+                direction.y = 0;
+                direction.Normalize();
 
-            transform.Translate(direction * Time.deltaTime * panSpeed * dir, Space.World);
-        }
+                transform.Translate(direction * Time.deltaTime * panSpeed * dir, Space.World);
+            }
 
-        // For Camera Rotation
-        if (Input.GetButton("Rotate") && PauseMenuManager.gamePause == false)
-        {
-            int dir = ((Input.GetAxisRaw("Rotate") > 0) ? 1 : -1);
-            transform.RotateAround(transform.position, Vector3.up, (dir * rotSpeed * Time.deltaTime));
-        }
+            // For Camera Rotation
+            if (Input.GetButton("Rotate"))
+            {
+                int dir = ((Input.GetAxisRaw("Rotate") > 0) ? 1 : -1);
+                transform.RotateAround(transform.position, Vector3.up, (dir * rotSpeed * Time.deltaTime));
+            }
 
-        // For Camera Zoom
-        if(Input.GetAxis("Mouse ScrollWheel") != 0 && PauseMenuManager.gamePause == false)
-        {
-            int dir = ((Input.GetAxisRaw("Mouse ScrollWheel") > 0) ? 1 : -1);
-            transform.Translate((Vector3.forward * dir) * Time.deltaTime * zoomSpeed * 15f);
+            // For Camera Zoom
+            if (Input.GetAxis("Mouse ScrollWheel") != 0 && transform.position.y > 1 && transform.position.y < 10)
+            {
+                int dir = ((Input.GetAxisRaw("Mouse ScrollWheel") > 0) ? 1 : -1);
+                transform.Translate((Vector3.forward * dir) * Time.deltaTime * zoomSpeed * 15f);
+
+                if (transform.position.y < 1)
+                    transform.Translate(0.0f, 1.0f, 0.0f);
+                else if (transform.position.y > 10)
+                    transform.Translate(0.0f, -1.0f, 0.0f);
+            }
         }
     }
 }
