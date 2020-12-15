28918d54d3080289c211e17d8c485677f2dd0e68
summerjw132 <summerjw@outlook.com>
2020-12-15 11:20:51 -0500

    Added Camp scene

---
+++ Thrill of the Hunt/Assets/Scripts/ButtonTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonTest : MonoBehaviour
+{
+
+    [SerializeField]
+    private string destinationSceneName = "";
+
+
+    public void PressMe()
+    {
+        Debug.Log("I wa pressed!");
+    }
+
+    public void GoToScene()
+    {
+#if UNITY_EDITOR 
+        EditorSceneManager.LoadScene(destinationSceneName);
+#else
+        SceneManager.LoadScene(destinationSceneName);
+#endif
+    }
+}
